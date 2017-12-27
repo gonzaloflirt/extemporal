1885233bc75ce97cb84019aec4f19042a7a6e1ba
gonzalo <gonzalo.flirt@gmail.com>
2017-12-27 21:50:28 +0100

    response2sc

--- response2sc.py
+++ response2sc.py
@@ -5,14 +5,14 @@ def readResponseFile(responseFile):
     with open (path.join(dataDir, responseFile + '.resp'), 'rb') as file:
         responses = pickle.load(file)
     for lang in responses:
-        for result in responses[lang].results:
-            alternative = result.alternatives[0]
-            confidence = alternative.confidence
-            for word_info in alternative.words:
-                word = word_info.word.replace('\'', '\\\'') \
+        for result in responses[lang]['results']:
+            alternative = result['alternatives'][0]
+            confidence = alternative['confidence']
+            for word_info in alternative['timestamps']:
+                word = word_info[0].replace('\'', '\\\'') \
                     .lower().encode('ascii', 'ignore').decode('utf-8')
-                start = word_info.start_time.seconds + word_info.start_time.nanos * 1e-9
-                end = word_info.end_time.seconds + word_info.end_time.nanos * 1e-9
+                start = word_info[1]
+                end = word_info[2]
                 length = end - start
                 scString = '(\'word\': \'' + word
                 scString += '\', \'start\': ' + str(start)
